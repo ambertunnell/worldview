6399a1ee17e8c83283bbf711aa0845f2d4f72b9a
Amber Tunnell <amber.tunnell@gmail.com>
2014-08-05 13:09:32 -0400

    adds articles.js

--- db/migrate/20140805142750_create_articles.rb
+++ db/migrate/20140805142750_create_articles.rb
@@ -1,7 +1,11 @@
 class CreateArticles < ActiveRecord::Migration
   def change
     create_table :articles do |t|
-      t.string :title
+      t.string   :title
+      t.text     :abstract
+      t.string   :url
+      t.string   :byline
+      t.datetime :pubdate
 
       t.timestamps
     end
