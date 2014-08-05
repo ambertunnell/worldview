40693fa3eb7468d805fa58629ab1f98b950b593a
Amber Tunnell <amber.tunnell@gmail.com>
2014-08-05 18:17:38 -0400

    save for pull

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,24 @@
 class UsersController < ApplicationController
 
   def index
-    @user = User.new
   end
 
-  def create
-  end
+  # def create
+  # end
 
-  def new
-  end
+  # def new
+  # end
 
-  def edit
-  end
+  # def edit
+  # end
 
   def show
-    @user = User.new
   end
 
-  def update
-  end
+  # def update
+  # end
 
-  def destroy
-  end
+  # def destroy
+  # end
 
 end
