8e7f1a0567b9161ec2e1a59aee17fab1c7838fd3
Amber Tunnell <amber.tunnell@gmail.com>
2014-08-05 13:48:52 -0400

    flesh out nyt ajax

--- app/assets/javascripts/articles.js
+++ app/assets/javascripts/articles.js
@@ -1,5 +1,7 @@
 $(function () {
 
+// $(".place").click(function () {
+
     var API_KEY = "b00e64b2e922c80462649603bea2f71f:19:31738630";
 
     var URL = "http://api.nytimes.com/svc/news/v3/content/all/world.jsonp?api-key=" + API_KEY;
@@ -10,11 +12,21 @@ $(function () {
         dataType: "jsonp",
         success: function (response) {
           console.log(response);
-            $('#news-results').append("<li>" + response + "</li>");
+
+          for (var i=0; i < 10; i++){  
+            var result = response.results[i];
+            var title = response.results[i].title;
+            var abstract = response.results[i].abstract;
+            var byline = response.results[i].byline;
+            var url = response.results[i].url;
+
+
+            $('#news-results').append("<div><h3>" + title + "</h3><p>" + abstract + "</p><p>" + "<a href='" + url + "'>Read more.</a></p></div>");
+          }
         },
         error: function (response) {
-            console.log("error");
-        }
-
+            console.log("NYT ajax query failed.");
+        } 
     });
+ // });   
  });    
