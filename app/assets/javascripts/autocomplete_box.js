9edb11377a63f9f38b0c48522068a4aad6f84bc9
kamoh <ben.serviss@gmail.com>
2014-08-07 22:59:39 -0400

    Tweaks to autocomplete

--- app/assets/javascripts/autocomplete_box.js
+++ app/assets/javascripts/autocomplete_box.js
@@ -2,9 +2,12 @@ $(function() {
 
   $('#autocomplete').autocomplete({
     serviceUrl: '/autocomplete/countries',
-    maxHeight: 10,
+    maxHeight: 200,
     minChars: 2,
+    dataType: 'text',
+    autoSelectFirst: true,
     deferRequestBy: 10,
+    // lookupFilter: function (suggestion, query, queryLowerCase) {},
     showNoSuggestionNotice: true,
     onSelect: function (suggestion) {
         console.log(suggestion);
