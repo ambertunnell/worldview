078b4443b508714ea907fd443861dd13dda403e2
Edward Warren <github email address>
2014-08-09 16:35:37 -0400

    working on replacing dom

--- app/assets/javascripts/clocks.js
+++ app/assets/javascripts/clocks.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
-
-    $('.clock-row').hide().fadeIn(2000);
+$('.clock-row').hide().fadeIn(2000);
 
     // script to type text in header
     var text = '         Click a clock.    ';
@@ -17,66 +16,77 @@ $(document).ready(function() {
     var d = new Date ();
     var n = d.getTimezoneOffset();
     var UTChour = d.getUTCHours();
+    
+
+//use $( "#clock-container " ).children('div').eq(0);
+   function makeClock ($sec,$min,$hour,coords){
+       var $clock1 = $("#clock-container > div")[0];
+       var $clock2 = $("#clock-container > div")[1];
+       var $clock3 = $("#clock-container > div")[2];
+       var $clock4 = $("#clock-container > div")[3];
+       var $clock5 = $("#clock-container > div")[4];
+
+
+        var WEATHER_API_KEY = 'c2ebf0ca079e86eb70261f70d92ce7ce';
+        var URL = "https://api.forecast.io/forecast/" + WEATHER_API_KEY + "/" + coords;
+        var offset;
+        weather = $.ajax({
+            url: URL,
+            dataType: 'jsonp',
+            success: function (response) {
+                offset = response['offset'];
+                setInterval( function() {
+                var seconds = new Date().getSeconds();
+                var sdegree = seconds * 6;
+                var srotate = "rotate(" + sdegree + "deg)";
+                $sec.css({"-moz-transform" : srotate, "-webkit-transform" : srotate});
+                }, 1000 );
+                
+                setInterval( function() {
+                var hours = UTChour + offset;
+                if (hours >= 24) {
+                  hour -= 24;
+                } else if (hours < 0) {
+                  hours -= -24;
+                };
+                // var hours = getTimezoneOffset
+                var mins = new Date().getMinutes();
+                var hdegree = hours * 30 + (mins / 2);
+                var hrotate = "rotate(" + hdegree + "deg)";
+                $hour.css({"-moz-transform" : hrotate, "-webkit-transform" : hrotate}); 
+                }, 1000 );
+
+                setInterval( function() {
+                var mins = new Date().getMinutes();
+                var mdegree = mins * 6;
+                var mrotate = "rotate(" + mdegree + "deg)";
+                $min.css({"-moz-transform" : mrotate, "-webkit-transform" : mrotate}); 
+                }, 1000 );
+            },
+              
+            error: function (response) {
+                console.log("Forecast.io API request failed in getting OFFSET");
+                
+            }
+            
+        });
+
+        
+        
+   }//end makeclock function
+
+  //new clock goes on left
+  //move all clocks right 
+  //make a new one on left  
+var nyc_coords = "40.712784,-74.005941";
 
     // NEW YORK
-
-    setInterval( function() {
-    var seconds = new Date().getSeconds();
-    var sdegree = seconds * 6;
-    var srotate = "rotate(" + sdegree + "deg)";
-    $(".nyc-clock .sec").css({"-moz-transform" : srotate, "-webkit-transform" : srotate});
-    }, 1000 );
+     makeClock($(".nyc-clock .sec"), $(".nyc-clock .min") ,$(".nyc-clock .hour"), nyc_coords);
     
-    setInterval( function() {
-    var hours = UTChour - 4;
-    if (hours >= 24) {
-      hour -= 24;
-    } else if (hours < 0) {
-      hours -= -24;
-    };
-    // var hours = getTimezoneOffset
-    var mins = new Date().getMinutes();
-    var hdegree = hours * 30 + (mins / 2);
-    var hrotate = "rotate(" + hdegree + "deg)";
-    $(".nyc-clock .hour").css({"-moz-transform" : hrotate, "-webkit-transform" : hrotate}); 
-    }, 1000 );
-
-    setInterval( function() {
-    var mins = new Date().getMinutes();
-    var mdegree = mins * 6;
-    var mrotate = "rotate(" + mdegree + "deg)";
-    $(".nyc-clock .min").css({"-moz-transform" : mrotate, "-webkit-transform" : mrotate}); 
-    }, 1000 );
 
     // SAN FRANCISCO
+     makeClock(-7,$(".sanfran-clock .sec"), $(".sanfran-clock .min") ,$(".sanfran-clock .hour"));
 
-    setInterval( function() {
-    var seconds = new Date().getSeconds();
-    var sdegree = seconds * 6;
-    var srotate = "rotate(" + sdegree + "deg)";
-    $(".sanfran-clock .sec").css({"-moz-transform" : srotate, "-webkit-transform" : srotate});  
-    }, 1000 );
-
-    setInterval( function() {
-    var hours = UTChour - 7;
-    if (hours >= 24) {
-      hours -= 24;
-    } else if (hours < 0) {
-      hours -= -24;
-    };
-    // var hours = new Date().getHours();
-    var mins = new Date().getMinutes();
-    var hdegree = hours * 30 + (mins / 2);
-    var hrotate = "rotate(" + hdegree + "deg)";
-    $(".sanfran-clock .hour").css({"-moz-transform" : hrotate, "-webkit-transform" : hrotate}); 
-    }, 1000 );
-
-    setInterval( function() {
-    var mins = new Date().getMinutes();
-    var mdegree = mins * 6;
-    var mrotate = "rotate(" + mdegree + "deg)";
-    $(".sanfran-clock .min").css({"-moz-transform" : mrotate, "-webkit-transform" : mrotate});
-    }, 1000 );
 
     // LONDON
 
