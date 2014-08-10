de7f616949202a9570814c7dea5df79b4f9b40d6
kamoh <ben.serviss@gmail.com>
2014-08-10 01:10:36 -0400

    Some work on validation flash message box for city input

--- app/assets/javascripts/new_city.js
+++ app/assets/javascripts/new_city.js
@@ -17,14 +17,23 @@
         url: "http://autocomplete.wunderground.com/aq?cb=city_results&query=" + user_input,
         dataType: 'jsonp',
         success: function(response) {
+          if (typeof response.RESULTS == 'undefined'){
+            $("#invalid-city").prepend("Not a valid city");
+          };
           var first_result = response.RESULTS[0].name;
           console.log(response);
           console.log(response.RESULTS);
           console.log("new city form success: " + first_result);
+          
+          $("#new-city").val("");
+          console.log("finished");
         },
         error: function(response) {
           // var first_result = response['results'][0]['name']
           console.log("new city form failure: " + response);
+          $("#invalid-city").prepend("Not a valid city");
+          $("#new-city").val("");
+          console.log("finished");
         }
       });
 
