50d08f4c8fad14c115626214b7ce04d30841d31a
Koren Leslie Cohen <koren.cohen@gmail.com>
2014-08-11 00:43:14 -0400

    working on footer and about

--- app/assets/javascripts/dividers.js
+++ app/assets/javascripts/dividers.js
@@ -2,10 +2,12 @@ $(function () {
 
   $('.divider').hide();
   $('.about').hide();
+  $('footer').hide();
 
   $(".clock-row").click(function () {
     $('.divider').show();
     $('.about').show();
+    $('footer').show();
   });
 
 });
