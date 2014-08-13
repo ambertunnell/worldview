090cbc0a97d87344484a5a4136131d13b8c80e03
Amber Tunnell <amber.tunnell@gmail.com>
2014-08-13 16:39:59 -0400

    trying to randomly fix stuff without knowing what im doing this is avi

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,18 +5,20 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if User.find_by_provider_and_uid(auth_hash[:provider], auth_hash[:uid])
-      @user = User.find_by_provider_and_uid(auth_hash[:provider], auth_hash[:uid])
-    else
-      @user = User.create_from_omniauth(auth_hash)
-      @user.cities << [City.find(1), City.find(2), City.find(3), City.find(4), City.find(5)]
-    end
+    if auth_hash.present?
+      if !@user = User.find_by_provider_and_uid(auth_hash[:provider], auth_hash[:uid])
+        @user = User.create_from_omniauth(auth_hash)
+        @user.cities << [City.find(1), City.find(2), City.find(3), City.find(4), City.find(5)]
+      end
 
-    if @user
-      session[:user_id] = @user.id
-      redirect_to root_url
+      if @user
+        session[:user_id] = @user.id
+        redirect_to root_url
+      else
+        redirect_to root_url
+      end
     else
-      redirect_to root_url
+      redirect_to root_url, :notice => "Please login via twitter"
     end
   end
 
