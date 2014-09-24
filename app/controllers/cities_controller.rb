3e044fd66add38f783be2886e7051a6a63b116eb
Edward Warren <github email address>
2014-09-24 15:10:15 -0400

    working. tracks temp user and will convert to a twitter user if they log in with twitter for first time

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,7 +1,7 @@
 class CitiesController < ApplicationController
 
   def create
-
+   
     @user = User.find(session[:user_id]) if session[:user_id]
     # binding.pry
     @city = City.find_or_create_by(lat: city_params[:lat], lon: city_params[:lon]) do |city| 
@@ -14,15 +14,17 @@ class CitiesController < ApplicationController
     end
 
     if !@user
-      # binding.pry
-      @user = User.create (name: 'Guest', provider: 'anon', uid: session[:user_id], image: 'https://origin.ih.constantcontact.com/fs197/1110193228531/img/301.jpg?a=1115291249439')
-      render json: @city
+      #binding.pry
+      @user = User.create(name: 'Guest', provider: 'anon', uid: session[:session_id], image: 'https://origin.ih.constantcontact.com/fs197/1110193228531/img/301.jpg?a=1115291249439')
+      @user.cities << [City.find(1), City.find(2), City.find(3), City.find(4), City.find(5)]
+      session[:user_id] = @user.id
     end
      
     unless @user.cities.find_by(lat: city_params[:lat], lon: city_params[:lon])
       # binding.pry
       @user.cities << @city if @user
       del_city_id = city_params[:lastClock].to_i
+     # binding.pry
       CityUser.find_by(user_id: @user.id, city_id: del_city_id).destroy if @user
       render json: @city
     else
@@ -34,7 +36,8 @@ class CitiesController < ApplicationController
 
   def get_city
     @user = User.find(session[:user_id]) if session[:user_id] 
-    if @user != nil
+    
+    if @user != nil && @user.provider != "anon"
       @user_info = { 
           articles: user_articles(current_user),
           photos: user_photos(current_user),
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,23 @@ class SessionsController < ApplicationController
   end
 
   def create
+    #binding.pry
     if auth_hash.present?
       if !@user = User.find_by_provider_and_uid(auth_hash[:provider], auth_hash[:uid])
-        @user = User.create_from_omniauth(auth_hash)
-        @user.cities << [City.find(1), City.find(2), City.find(3), City.find(4), City.find(5)]
+        if session[:user_id] #ie if was a temp user & this is first time authing with twitter 
+          #then replace their temp account uid and provider with this new auth hash info. The session will only have a user_id for a temp user or for someone already logged in. If already logged in then would never end up in this create method so this should only catch temp users who log in for first time
+         
+          @user = User.find(session[:user_id])
+          
+          @user.uid = auth_hash[:uid]
+          @user.provider = auth_hash[:provider]
+          @user.name = auth_hash[:info][:name]
+          @user.image = auth_hash[:info][:image]
+          @user.save
+        else
+          @user = User.create_from_omniauth(auth_hash)
+          @user.cities << [City.find(1), City.find(2), City.find(3), City.find(4), City.find(5)]
+        end
       end
 
       if @user
